ef7a59b3eca1df80b0a0e2bd33d19a625682dcca
Diego Medina <diego@fmpwizard.com>
2015-04-23 23:20:51 -0400

    Sample ajax form with checkboxes, radio buttons, text, drop down menus

--- src/main/scala/code/model/User.scala
+++ src/main/scala/code/model/User.scala
@@ -4,30 +4,20 @@ package model
 import net.liftweb.mapper._
 import net.liftweb.util._
 import net.liftweb.common._
-import net.liftweb.sitemap.Loc._
-import net.liftweb.http._
-import net.liftweb.http.SHtml._
-import net.liftmodules.FoBoBs.mapper._
 
 /**
  * The singleton that has methods for accessing the database
  */
-object User extends User with MetaMegaProtoUser[User] with BootstrapMegaMetaProtoUser[User]  {
+object User extends User with MetaMegaProtoUser[User] {
   override def dbTableName = "users" // define the DB table name
   override def screenWrap = Full(<lift:surround with="default" at="content">
 			       <lift:bind /></lift:surround>)
   // define the order fields will appear in forms and output
   override def fieldOrder = List(id, firstName, lastName, email,
-  locale, timezone, password, textArea) 
-  
+  locale, timezone, password, textArea)
+
   // comment this line out to require email validations
   override def skipEmailValidation = true
-
-  //add a loc group to the user menu
-  override def globalUserLocParams: List[LocParam[Unit]] = List(LocGroup("user"))
-  override def resetPasswordMenuLoc: Box[net.liftweb.sitemap.Menu] = Box(Empty) 
-  override def validateUserMenuLoc: Box[net.liftweb.sitemap.Menu] = Box(Empty) 
- 
 }
 
 /**
--- src/main/scala/code/snippet/HelloWorld.scala
+++ src/main/scala/code/snippet/HelloWorld.scala
@@ -1,23 +1,51 @@
 package code 
-package snippet 
+package snippet
 
-import scala.xml.{NodeSeq, Text}
-import net.liftweb.util._
+import net.liftweb.util.Helpers._
 import net.liftweb.common._
-import java.util.Date
-import code.lib._
-import Helpers._
+import net.liftweb.http.SHtml
+import net.liftweb.http.js.JsCmd
+import net.liftweb.http.js.JsCmds.SetHtml
+import xml.Text
 
 class HelloWorld {
-  lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
+  import BirthGender._
 
-  // replace the contents of the element with id "time" with the date
-  def howdy = "#time *" #> date.map(_.toString)
+  def render = {
 
-  /*
-   lazy val date: Date = DependencyFactory.time.vend // create the date via factory
+    var name = ""
+    var old = false
+    var age = 5
+    var sex: Box[BirthGender] = Full(BirthGender.NotSpecified)
 
-   def howdy = "#time *" #> date.toString
-   */
+    val ages = List(1,2,3,4,5,6,7,8,9,10)
+
+    val options : Seq[BirthGender] = BirthGender.values.toSeq
+
+    val radio : SHtml.ChoiceHolder[BirthGender] =
+      SHtml.radioElem(options, sex) { selected =>
+        sex = selected
+      }
+
+    def process() : JsCmd = {
+      SetHtml("name", Text(name)) &
+      SetHtml("age", Text(age.toString)) &
+      SetHtml("sex", Text(sex.getOrElse(BirthGender.NotSpecified).toString)) &
+      SetHtml("old", Text(old.toString))
+    }
+
+    "@name" #> SHtml.text(name, s => name = s) &
+    "@old" #> SHtml.checkbox(old, s => old = s) &
+    "@age" #> SHtml.selectElem(ages, Some(age))(age = _) &
+    ".options" #> radio.toForm &
+    "type=submit" #> SHtml.ajaxSubmit("Click Me", process)
+  }
+
 }
 
+object BirthGender extends Enumeration {
+  type BirthGender = Value
+  val Male = Value("Male")
+  val Female = Value("Female")
+  val NotSpecified = Value("Rather Not Say")
+}
